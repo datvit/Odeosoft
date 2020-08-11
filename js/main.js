c16c5c32f391e11dda4d1a79779ccecc1a302830
datvit <davit.abjandadze05@geolab.edu.ge>
2020-08-11 17:48:37 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $('.search-btn').on('click', function() {
+    $('.search').toggleClass("active");
+  });
+
+  $('.burger').on('click', function() {
+    $(this).toggleClass("active");
+  });
+
+  });
